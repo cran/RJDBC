bc998d023c7699f475bdab7b16434f66e20631d7
Matt Summersgill <matt.summersgill@hpidc.com>
2017-08-23 14:38:08 -0500

    Probably going to break everything

--- R/class.R
+++ R/class.R
@@ -289,16 +289,23 @@ setMethod("fetch", signature(res="JDBCResult", n="numeric"), def=function(res, n
   block <- as.integer(block)
   if (length(block) != 1L) stop("invalid block size")
   if (cols < 1L) return(NULL)
-  l <- list()
+  
+  
+  l_template <- list() ## Original allocation of l
+  
+  l_container <- list()
+  length(l_container) <- 1024 ## Allocate a length for the number of blocks it might take to pull back
+  l_container_used_elements <- 0L
+  
   cts <- rep(0L, cols)
   for (i in 1:cols) {
     ct <- .jcall(res@md, "I", "getColumnType", i)
     if (ct == -5 | ct ==-6 | (ct >= 2 & ct <= 8)) {
-      l[[i]] <- numeric()
+      l_template[[i]] <- numeric()
       cts[i] <- 1L
     } else
-      l[[i]] <- character()
-    names(l)[i] <- .jcall(res@md, "S", "getColumnName", i)
+      l_template[[i]] <- character()
+    names(l_template)[i] <- .jcall(res@md, "S", "getColumnName", i)
   }
   rp <- res@pull
   if (is.jnull(rp)) {
@@ -306,22 +313,28 @@ setMethod("fetch", signature(res="JDBCResult", n="numeric"), def=function(res, n
     .verify.JDBC.result(rp, "cannot instantiate JDBCResultPull hepler class")
   }
   if (n < 0L) { ## infinite pull
-    print(length(l))
     stride <- 32768L  ## start fairly small to support tiny queries and increase later
     while ((nrec <- .jcall(rp, "I", "fetch", stride, block)) > 0L) {
+      
+      l_container_used_elements <- l_container_used_elements + 1L
+      print(l_container_used_elements)
+      l <- l_template
+      
       for (i in seq.int(cols))
-        l[[i]] <- c(l[[i]], if (cts[i] == 1L) .jcall(rp, "[D", "getDoubles", i) else .jcall(rp, "[Ljava/lang/String;", "getStrings", i))
+        l[[i]] <- if (cts[i] == 1L) .jcall(rp, "[D", "getDoubles", i) else .jcall(rp, "[Ljava/lang/String;", "getStrings", i)
+      l_container[[l_container_used_elements]] <- l
       if (nrec < stride) break
       stride <- 524288L # 512k
     }
   } else {
     nrec <- .jcall(rp, "I", "fetch", as.integer(n), block)
     for (i in seq.int(cols)) l[[i]] <- if (cts[i] == 1L) .jcall(rp, "[D", "getDoubles", i) else .jcall(rp, "[Ljava/lang/String;", "getStrings", i)
+    l_container[[l_container_used_elements]] <- l
   }
-  # as.data.frame is expensive - create it on the fly from the list
-  attr(l, "row.names") <- c(NA_integer_, length(l[[1]]))
-  class(l) <- "data.frame"
-  l
+  ## as.data.frame is expensive - create it on the fly from the list
+  # attr(l, "row.names") <- c(NA_integer_, length(l[[1]]))
+  # class(l) <- "data.frame"
+  l_container[[1:l_container_used_elements]]
 })
 
 setMethod("dbClearResult", "JDBCResult",
